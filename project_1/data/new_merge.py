1cccbba87cf94fe09dc4a1dcbc50642c9aec2a92
Tiago Azevedo <up202108699@edu.fe.up.pt>
2024-11-05 11:44:56 +0000

    add more diseases

---
+++ project_1/data/new_merge.py
@@ -0,0 +1,25 @@
+from utils import *
+
+def merge(wikidata_content, wikipedia_content):
+    wikipedia_diseases = {disease.lower(): disease for disease in wikipedia_content.keys()}
+
+    for wikidata_disease, wikidata_information in wikidata_content.items():
+        if wikidata_disease.lower() in wikipedia_diseases:
+            disease = wikipedia_diseases[wikidata_disease.lower()]
+            merge_data(wikidata_information, wikipedia_content[disease])
+
+    return wikipedia_content
+    
+
+def merge_data(wikidata_disease_information, wikipedia_disease_information):
+    for k,v in wikidata_disease_information.items():
+        wikipedia_disease_information.update({k: v})
+
+
+if __name__ == "__main__":
+    wikidata_content = load_json("./wikidata/wikidata.json")
+    wikipedia_content = load_json("./wikidata/New/wikipedia_complete.json")
+
+    content = merge(wikidata_content, wikipedia_content)
+
+    save_json("data_complete.json", content)
---
+++ project_1/data/wikipedia/New/count.py
@@ -0,0 +1,11 @@
+import json
+import re
+
+def load_json(file_name):
+    f = open(file_name, "r", encoding="UTF-8")
+    return json.load(f)
+
+if __name__ == "__main__":
+    data = load_json("../../data_complete.json")
+    print(len(data))
+
