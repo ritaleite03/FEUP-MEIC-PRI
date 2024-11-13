db2c8b68912d07f2a3fec767bf8b1797ebbe6965
joaonevesf <joaodnfernandes99@gmail.com>
2024-11-13 18:04:38 +0000

    added synonyms and stopwords to setup.sh

--- project_1/solr/setup.sh
+++ project_1/solr/setup.sh
@@ -13,6 +13,12 @@ docker exec pri_proj solr create_core -c diseases
 
 curl http://localhost:8983/solr/diseases/config -d '{"set-user-property": {"update.autoCreateFields":"false"}}'
 
+docker cp ../solr/stopwords.txt pri_proj:/var/solr/data/diseases/conf
+sleep 1
+
+docker cp ../solr/synonyms.txt pri_proj:/var/solr/data/diseases/conf
+sleep 1
+
 curl -X POST -H 'Content-type:application/json' \
   --data-binary "@../solr/schema.json" \
   http://localhost:8983/solr/diseases/schema
