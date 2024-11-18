40a83be8d671a5094d83e3cc483cfb43aaa8401c
Rita Leite <ritaicleite@gmail.com>
2024-11-18 09:10:58 +0000

    Star query and change schema

--- project_1/solr/setup.sh
+++ project_1/solr/setup.sh
@@ -9,16 +9,16 @@ sleep 1
 
 docker exec pri_proj solr create_core -c diseases
 
-curl http://localhost:8983/solr/diseases/config -d '{"set-user-property": {"update.autoCreateFields":"false"}}'
+# curl http://localhost:8983/solr/diseases/config -d '{"set-user-property": {"update.autoCreateFields":"false"}}'
 
-docker cp ../solr/stopwords.txt pri_proj:/var/solr/data/diseases/conf
-sleep 1
-
-docker cp ../solr/synonyms.txt pri_proj:/var/solr/data/diseases/conf
-sleep 1
+#docker cp ../solr/stopwords.txt pri_proj:/var/solr/data/diseases/conf
+#sleep 1
+#
+#docker cp ../solr/synonyms.txt pri_proj:/var/solr/data/diseases/conf
+#sleep 1
 
 curl -X POST -H 'Content-type:application/json' \
-    --data-binary "@../solr/schema.json" \
+    --data-binary "@../solr/simple_schema.json" \
     http://localhost:8983/solr/diseases/schema
 
 docker exec -it pri_proj bin/solr post -c diseases /data/solr_data.json
