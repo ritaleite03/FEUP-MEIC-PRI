eabd2bfb8bf06272409e74b408a55581d116db74
joaonevesf <joaodnfernandes99@gmail.com>
2024-11-18 10:30:36 +0000

    added 2 cores to setup

--- project_1/solr/setup.sh
+++ project_1/solr/setup.sh
@@ -7,18 +7,28 @@ docker rm pri_proj
 docker run -p 8983:8983 --name pri_proj -v ${PWD}:/data -d solr:9
 sleep 1
 
-docker exec pri_proj solr create_core -c diseases
-
-curl http://localhost:8983/solr/diseases/config -d '{"set-user-property": {"update.autoCreateFields":"false"}}'
-
-#docker cp ../solr/stopwords.txt pri_proj:/var/solr/data/diseases/conf
-#sleep 1
+# docker exec pri_proj solr create_core -c diseases
 #
-#docker cp ../solr/synonyms.txt pri_proj:/var/solr/data/diseases/conf
-#sleep 1
+# curl http://localhost:8983/solr/diseases/config -d '{"set-user-property": {"update.autoCreateFields":"false"}}'
+#
+# docker cp ../solr/stopwords.txt pri_proj:/var/solr/data/diseases/conf
+# sleep 1
+#
+# docker cp ../solr/synonyms.txt pri_proj:/var/solr/data/diseases/conf
+# sleep 1
+#
+# curl -X POST -H 'Content-type:application/json' \
+#     --data-binary "@schema.json" \
+#     http://localhost:8983/solr/diseases/schema
+
+# docker exec -it pri_proj bin/solr post -c diseases /data/solr_data.json
+
+docker exec pri_proj solr create_core -c diseases_simple
+
+curl http://localhost:8983/solr/diseases_simple/config -d '{"set-user-property": {"update.autoCreateFields":"false"}}'
 
 curl -X POST -H 'Content-type:application/json' \
-    --data-binary "@../solr/simple_schema.json" \
-    http://localhost:8983/solr/diseases/schema
+    --data-binary "@simple_schema.json" \
+    http://localhost:8983/solr/diseases_simple/schema
 
-docker exec -it pri_proj bin/solr post -c diseases /data/solr_data.json
+docker exec -it pri_proj bin/solr post -c diseases_simple /data/solr_data.json
