1da0abcf6214eb14cbb775b372e9c89a327b9027
Rita Leite <ritaicleite@gmail.com>
2024-11-18 11:20:38 +0000

    change synonyms

--- project_1/solr/setup.sh
+++ project_1/solr/setup.sh
@@ -7,21 +7,21 @@ docker rm pri_proj
 docker run -p 8983:8983 --name pri_proj -v ${PWD}:/data -d solr:9
 sleep 1
 
-# docker exec pri_proj solr create_core -c diseases
-#
-# curl http://localhost:8983/solr/diseases/config -d '{"set-user-property": {"update.autoCreateFields":"false"}}'
-#
-# docker cp ../solr/stopwords.txt pri_proj:/var/solr/data/diseases/conf
-# sleep 1
-#
-# docker cp ../solr/synonyms.txt pri_proj:/var/solr/data/diseases/conf
-# sleep 1
-#
-# curl -X POST -H 'Content-type:application/json' \
-#     --data-binary "@schema.json" \
-#     http://localhost:8983/solr/diseases/schema
+docker exec pri_proj solr create_core -c diseases
 
-# docker exec -it pri_proj bin/solr post -c diseases /data/solr_data.json
+curl http://localhost:8983/solr/diseases/config -d '{"set-user-property": {"update.autoCreateFields":"false"}}'
+
+docker cp ../solr/stopwords.txt pri_proj:/var/solr/data/diseases/conf
+sleep 1
+
+docker cp ../solr/synonyms.txt pri_proj:/var/solr/data/diseases/conf
+sleep 1
+
+curl -X POST -H 'Content-type:application/json' \
+     --data-binary "@schema.json" \
+     http://localhost:8983/solr/diseases/schema
+
+docker exec -it pri_proj bin/solr post -c diseases /data/solr_data.json
 
 docker exec pri_proj solr create_core -c diseases_simple
 
