4c73d62acd1650d53c023add250776b67ff137cf
Rita Leite <ritaicleite@gmail.com>
2024-12-06 14:38:15 +0000

    Setup milestone 3

--- project_1/solr/setup.sh
+++ project_1/solr/setup.sh
@@ -12,6 +12,7 @@ docker exec pri_proj solr create_core -c diseases
 curl http://localhost:8983/solr/diseases/config -d '{"set-user-property": {"update.autoCreateFields":"false"}}'
 
 docker cp ../solr/stopwords.txt pri_proj:/var/solr/data/diseases/conf
+
 sleep 1
 
 curl -X POST -H 'Content-type:application/json' \
@@ -29,3 +30,13 @@ curl -X POST -H 'Content-type:application/json' \
     http://localhost:8983/solr/diseases_simple/schema
 
 docker exec -it pri_proj bin/solr post -c diseases_simple /data/solr_data.json
+
+docker exec pri_proj solr create_core -c diseases_semantic
+
+curl http://localhost:8983/solr/diseases_semantic/config -d '{"set-user-property": {"update.autoCreateFields":"false"}}'
+
+curl -X POST -H 'Content-type:application/json' \
+    --data-binary "@semantic_schema.json" \
+    http://localhost:8983/solr/diseases_semantic/schema
+
+docker exec -it pri_proj bin/solr post -c diseases_semantic /data/semantic_courses.json
