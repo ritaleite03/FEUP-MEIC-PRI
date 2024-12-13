25dbab231649ad778aaddb89aa0af49822cc1cfc
Rita Leite <ritaicleite@gmail.com>
2024-12-13 14:49:49 +0000

    Start fetch

--- project_1/app/src/App.tsx
+++ project_1/app/src/App.tsx
@@ -8,23 +8,15 @@ function App() {
     const [inputValue, setInputValue] = useState("");
     const [diseases, setDiseases] = useState<Array<Record<string, any>>>([]);
 
-    const handleSubmit = (event: any) => {
+    const handleSubmit = async (event: any) => {
         event.preventDefault();
-        setDiseases([
-            {
-                name: "Aliquam maximus molestie arcu",
-                overview:
-                    "Morbi tincidunt vitae arcu rutrum tincidunt. Cras nec ligula odio. Nullam sagittis dolor nec magna luctus posuere. Praesent lectus elit, dignissim sit amet nisl non, fringilla sodales augue. Pellentesque iaculis massa in vestibulum hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla facilisi. Praesent rutrum ultrices nisl, at rutrum ex. Duis eleifend malesuada dui iaculis elementum. Morbi sem nibh, pulvinar quis luctus ut, auctor a leo. Vivamus a libero posuere, mollis turpis a, faucibus tortor. Quisque sed lectus id nisl pharetra molestie in id elit. Aliquam at lectus ut nulla lobortis bibendum. Ut purus tortor, interdum eget dui eleifend, tincidunt eleifend purus. Aliquam id est molestie, vehicula ipsum quis, euismod nulla. In dolor urna, consectetur ut sodales eget, malesuada nec est. ",
-            },
-            {
-                name: "Mauris euismod pretium justo non",
-                overview:
-                    " In eleifend ut diam nec euismod. Morbi lorem orci, blandit nec lectus ac, blandit mollis nisl. Vestibulum lobortis nisl vel sem placerat pretium. Cras vel sapien placerat, sagittis odio eu, lacinia massa. Nulla bibendum urna est, quis pellentesque magna convallis ut. Pellentesque vehicula, turpis eget ultrices mattis, purus enim scelerisque ex, cursus egestas metus est vel nulla. Mauris at tincidunt justo, id malesuada turpis. Mauris ut placerat leo. ",
-            },
-            {
-                name: "Fusce luctus accumsan velit efficitur",
-            },
-        ]);
+        const response = await fetch("http://localhost:5001/projects");
+        if (!response.ok) {
+            throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        } else {
+            const data = await response.json();
+            setDiseases(data);
+        }
     };
 
     return (
